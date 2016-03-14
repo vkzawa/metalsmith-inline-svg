def12b12424f00060ff6ff26e94152d33d5ac37b
Mikael FIMA <mika@verylastroom.com>
2016-03-14 18:42:35 +0100

    Could run inline on no-defs svg file

--- test/index.js
+++ test/index.js
@@ -13,4 +13,14 @@ describe('metalsmith-inline-svg', function () {
         done();
       })
   });
+
+  it('should inline the svg with no defs', function (done) {
+    Metalsmith('test/fixtures/no-defs')
+      .use(inlineSvg())
+      .build(function (err) {
+        if (err) return done(err);
+        assertDir('test/fixtures/no-defs/expected', 'test/fixtures/no-defs/build')
+        done();
+      })
+  });
 })
